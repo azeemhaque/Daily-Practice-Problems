9068dbc1335a395a7fb2922ff1f40c43cde6a09f
Azeem Haque <91793824+azeemhaque@users.noreply.github.com>
2023-10-13 10:01:21 +0530

    Time: 36 ms (48.87%), Space: 13.5 MB (19.91%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,8 +1,12 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        for(var i = cost.length - 3; i>=0; i--)
-            cost[i] = cost[i] + Math.min(cost[i+1], cost[i+2]);
-        
-        return Math.min(cost[0], cost[1]);
-    }
-}
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        c1 = cost[0]
+        c2 = cost[1]
+        for i in range(2,len(cost)):
+            c1,c2 = c2, min(c1,c2) + cost[i]
+            
+        return min(c1,c2)
