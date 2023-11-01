61ba3332c13e668603eb7fe9fa40ccd4da67b5ec
Azeem Haque <91793824+azeemhaque@users.noreply.github.com>
2023-11-01 20:54:37 +0530

    Time: 55 ms (63.26%), Space: 20.3 MB (86.51%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        def inorder_traversal(node):
+            nonlocal prev_node , max_count,curr_count,modes
+            if not node:
+                return
+            inorder_traversal(node.left)
+
+            if node.val == prev_node:
+                curr_count+=1
+            else:
+                curr_count=1
+            if curr_count > max_count:
+                max_count=curr_count
+                modes=[node.val]
+            elif curr_count==max_count:
+                modes.append(node.val)
+            prev_node=node.val
+            inorder_traversal(node.right)
+            
+        prev_node=None
+        max_count=0
+        curr_count=0
+        modes=[]
+        inorder_traversal(root)
+        return modes
